3b2587b28c02bd94da091649ac645e689cd43686
RailonA <railonacosta@gmail.com>
2021-01-26 00:26:35 -0500

    Add Readme.md information

--- bot.rb
+++ bot.rb
@@ -7,10 +7,10 @@ require '../lib/user_info'
 
 
   twitter = Twitter::REST::Client.new do |config|
-    # config.consumer_key        = "YOUR_CONSUMER_KEY"
-    # config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-    # config.access_token        = "YOUR_ACCESS_TOKEN"
-    # config.access_token_secret = "YOUR_ACCESS_SECRET"
+    config.consumer_key        = "YOUR_CONSUMER_KEY"
+    config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+    config.access_token        = "YOUR_ACCESS_TOKEN"
+    config.access_token_secret = "YOUR_ACCESS_SECRET"
 
   end
   @time = Time.now
