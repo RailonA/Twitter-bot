5cc9429e8301295fc41c623a6f171a298c44ad8f
RailonA <railonacosta@gmail.com>
2021-01-26 11:53:53 -0500

    Fix linters

--- bin/bot.rb
+++ bin/bot.rb
@@ -3,27 +3,31 @@
 require 'twitter'
 require 'date'
 
-twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_ACCESS_SECRET"
+# tweet_info = TweetInfo.new
+
+@twitter = Twitter::REST::Client.new do |config|
+  #   config.consumer_key = 'YOUR_CONSUMER_KEY'
+  #   config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+  #   config.access_token = 'YOUR_ACCESS_TOKEN'
+  #   config.access_token_secret = 'YOUR_ACCESS_SECRET'
+  config.consumer_key = 'VL976Jq6COpnO4FhBJte6qMMO'
+  config.consumer_secret = 'lUAlVNgckSdQ1DU6HSpPy4M2bipjm6UWCqozPtzD3x3wem1H4y'
+  config.access_token = '1297192003965460486-EBPDzV4STHT3UFlg8UYe2Y3JbaSsRu'
+  config.access_token_secret = 'UgHJi7q9htKpUxTEbvDRrd3CqNwPlU8CCX3878yI54Psw'
 end
 
 @time = Time.now
-
-@randome_page = 0
-@randome_page_tweet = 0
-@selected_info = ''
-@option_data_name = ['', '', '']
 @memory = []
+@selected_info = ''
+@option_data_name = []
+
 @total_count = 10
 
-page_one = twitter.user_timeline('SpaceX', count: @total_count)
-page_two = twitter.user_timeline('rails', count: @total_count)
-page_three = twitter.user_timeline('arduino', count: @total_count)
-
 @option_data_name = %w[SpaceX rails arduino]
+
+page_one = @twitter.user_timeline(@option_data_name[0], count: @total_count)
+page_two = @twitter.user_timeline(@option_data_name[1], count: @total_count)
+page_three = @twitter.user_timeline(@option_data_name[2], count: @total_count)
 
 @option_data = [page_one, page_two, page_three]
 
@@ -39,22 +43,21 @@ def selected_tweet
   @option_data[set_randome_page][set_randome_page_tweet].full_text
 end
 
-def daily_print
-  #   puts "Fascinating stuff done by @#{@option_data_name[@randome_page]}" + selected_tweet
-  twitter.update("Fascinating stuff done by @#{@option_data_name[set_randome_page]}" + selected_tweet)
+def tweet_update
+#   puts "Fascinating stuff done by @#{@option_data_name[set_randome_page]}" + selected_tweet
+  @twitter.update("Fascinating stuff done by @#{@option_data_name[@randome_page]}" + selected_tweet)
 end
 
-def memory
-  while (@time.hour == 12) && @time.min.zero?
-
+def memory_checker
+  while (@time.hour == 9) && @time.min.zero?
     if @memory.include?(selected_tweet)
+      set_randome.reset
       set_randome_page.reset
-      set_randome_page_tweet.reset
     else
       @memory.push(selected_tweet)
-      daily_print
+      tweet_update
     end
   end
 end
 
-memory
+memory_checker
