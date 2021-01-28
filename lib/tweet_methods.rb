b85a7248fe91725f0a09ea3d919cb76f2994e9f7
RailonA <railonacosta@gmail.com>
2021-01-28 11:24:15 -0500

    Fixed all linters

--- lib/tweet_methods.rb
+++ lib/tweet_methods.rb
@@ -9,10 +9,10 @@ class TweetBotMethods
   attr_accessor :tweet
 
   @tweet = Twitter::REST::Client.new do |config|
-      config.consumer_key = 'YOUR_CONSUMER_KEY'
-      config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-      config.access_token = 'YOUR_ACCESS_TOKEN'
-      config.access_token_secret = 'YOUR_ACCESS_SECRET'
+    config.consumer_key = 'YOUR_CONSUMER_KEY'
+    config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+    config.access_token = 'YOUR_ACCESS_TOKEN'
+    config.access_token_secret = 'YOUR_ACCESS_SECRET'
   end
 
   @time = Time.new
