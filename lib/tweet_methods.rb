8c10f2000282575b34ecd59c92262829793f5552
RailonA <railonacosta@gmail.com>
2021-01-28 11:20:07 -0500

    Fix removed my keys

--- lib/tweet_methods.rb
+++ lib/tweet_methods.rb
@@ -9,14 +9,10 @@ class TweetBotMethods
   attr_accessor :tweet
 
   @tweet = Twitter::REST::Client.new do |config|
-    #   config.consumer_key = 'YOUR_CONSUMER_KEY'
-    #   config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-    #   config.access_token = 'YOUR_ACCESS_TOKEN'
-    #   config.access_token_secret = 'YOUR_ACCESS_SECRET'
-    config.consumer_key = 'VL976Jq6COpnO4FhBJte6qMMO'
-    config.consumer_secret     = 'lUAlVNgckSdQ1DU6HSpPy4M2bipjm6UWCqozPtzD3x3wem1H4y'
-    config.access_token        = '1297192003965460486-EBPDzV4STHT3UFlg8UYe2Y3JbaSsRu'
-    config.access_token_secret = 'UgHJi7q9htKpUxTEbvDRrd3CqNwPlU8CCX3878yI54Psw'
+      config.consumer_key = 'YOUR_CONSUMER_KEY'
+      config.consumer_secret = 'YOUR_CONSUMER_SECRET'
+      config.access_token = 'YOUR_ACCESS_TOKEN'
+      config.access_token_secret = 'YOUR_ACCESS_SECRET'
   end
 
   @time = Time.new
